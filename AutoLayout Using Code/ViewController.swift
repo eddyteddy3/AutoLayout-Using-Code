d445c3d22b429822fabd6c010041899ddb38bbe9
Mouzzam <moazzamtahir81@gmail.com>
2019-07-28 20:37:54 +0500

    button and labels are added to the view but no constraints have been implemented yet.

--- AutoLayout Using Code/ViewController.swift
+++ AutoLayout Using Code/ViewController.swift
@@ -12,9 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        createLayout()
     }
-
+    
+    //creating a function in which we will implement autolayout using code to the view
+    func createLayout() {
+        //initialing an instance of superview
+        let superView = self.view
+        
+        //declaring a button
+        let button = UIButton()
+        button.setTitle("Click Here", for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        button.translatesAutoresizingMaskIntoConstraints = false //this is false because we are implementing our own constraints otherwise conflicts will arise.
+        
+        //declaring the label
+        let label = UILabel()
+        label.text = "Hello, Swift!"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        superView?.addSubview(button)
+        superView?.addSubview(label)
+    }
 
 }
 
